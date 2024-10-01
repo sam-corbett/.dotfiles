3ff198933eaf0bb3d9dad3e846ca6ace323dc5b4
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2024-10-01 09:48:10 -0400

    updated lua files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,20 @@
 -- luacheck: ignore vim
-vim.cmd("set encoding=utf-8")
-vim.cmd("set termguicolors")
+require("config.lazy")
 
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-vim.cmd("set cursorline")
-vim.cmd("set numberwidth=4")
-vim.cmd("set tabstop=2")
-vim.cmd("set expandtab")
-vim.cmd("set smartindent")
-vim.cmd("set colorcolumn=80")
+-- lazy nvim
+require ("user.options")
 
-vim.cmd("set scrolloff=6")
-vim.cmd("set wrap!")
-vim.cmd("set belloff=all")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd("set listchars=space:·,eol:↩")
-vim.cmd("set list")
+require("lazy").setup("plugins")

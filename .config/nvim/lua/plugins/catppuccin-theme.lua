34160b4330bcb507fdb6bc7afd6c8d36186a7d6e
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2024-10-03 09:48:19 -0400

    added more lua files

---
+++ .config/nvim/lua/plugins/catppuccin-theme.lua
@@ -0,0 +1,10 @@
+-- luacheck: ignore vim
+return {
+    "catppuccin/nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("catppuccin").setup({})
+      vim.cmd('colorscheme catppuccin-mocha')
+    end
+  }
